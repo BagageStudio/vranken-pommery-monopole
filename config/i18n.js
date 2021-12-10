2b29f4c9459ece1474454eae83f98f286223bd86
menthe <adrien.le.mentheour@gmail.com>
2021-12-10 14:34:17 +0100

    🌐 fr default

--- config/i18n.js
+++ config/i18n.js
@@ -1,28 +1,33 @@
 import { routes } from '../app/crawler/routes';
 
-export const defaultLocale = 'en';
+export const defaultLocale = 'fr';
 
 export const locales = [
-    { code: 'en', iso: 'en', name: 'english' },
-    { code: 'fr', iso: 'fr', name: 'français' }
+    { code: 'fr', iso: 'fr', name: 'français' },
+    { code: 'en', iso: 'en', name: 'english' }
 ];
 
 // 🚦 Specific routes
 // NOTE: You can use isProdEnv to set conditionnal routes (not showing on prod for example)
 export const getPagesList = () => ({
     [routes.home.i18nFormat]: {
+        fr: '/',
         en: '/'
     },
     [routes.page.i18nFormat]: {
+        fr: '/:slug?',
         en: '/:slug?'
     },
     [routes.cuvee.i18nFormat]: {
+        fr: '/:brand?/:cuvee?',
         en: '/:brand?/:cuvee?'
     },
     [routes.category.i18nFormat]: {
+        fr: '/:brand?/:cuvee?/:category?',
         en: '/:brand?/:cuvee?/:category?'
     },
     [routes.product.i18nFormat]: {
+        fr: '/:brand?/:cuvee?/:category?/:uuid?',
         en: '/:brand?/:cuvee?/:category?/:uuid?'
     }
 });
