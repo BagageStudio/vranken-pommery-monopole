33557d069f653d082695553f678e2a910b6b9550
menthe <adrien.le.mentheour@gmail.com>
2021-12-13 08:09:45 +0100

    👷‍♂️ page

---
+++ pages/_slug.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <component :is="template" v-if="template" :data="data" />
+    </div>
+</template>
+
+<script>
+import { camalize, pascalize } from '@stereorepo/sac';
+import { getIso, getSlug, setRouteParams } from '~/api/dato/helpers';
+
+import slugToModelApiKey from '~/api/dato/helpers/slugToModelApiKey.json';
+import { getQuery } from '~/api/dato';
+import handleSeo from '~/app/seo';
+
+import Page from '~/components/Templates/Page';
+
+export default {
+    components: {
+        Page
+    },
+    async asyncData(context) {
+        const { $dato, error, route } = context;
+        const finalData = {};
+
+        // Getting the right locale iso
+        const lang = getIso.call(context);
+
+        // Getting the slug
+        const slug = getSlug.call(context);
+
+        // Ici ton _modelApiKey
+        finalData.template = slugToModelApiKey[lang][slug];
+
+        try {
+            const { data } = await $dato
+                .post('/', { query: getQuery(finalData.template), variables: { lang, slug } })
+                .then(({ data }) => data);
+
+            finalData.data = data[camalize(finalData.template)];
+            finalData.seo = handleSeo({ route: route.fullPath, seo: finalData.data.seo, lang });
+            finalData.template = pascalize(finalData.template);
+        } catch (e) {
+            return error({ statusCode: 404, message: e });
+        }
+
+        // Getting raw slugs for the current page from Dato
+        const datoLocales = finalData.data._allSlugLocales;
+        await setRouteParams.call(context, datoLocales);
+
+        return finalData;
+    },
+    data: () => ({
+        data: {},
+        seo: {}
+    })
+};
+</script>
