c413b9d949aae7c25bbbe6db526521e5698541c7
menthe <adrien.le.mentheour@gmail.com>
2021-12-13 17:57:17 +0100

    🍱 svgs

--- components/Layout/Svgs.vue
+++ components/Layout/Svgs.vue
@@ -6,6 +6,12 @@
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
     >
-        <defs></defs>
+        <defs>
+            <symbol id="icon-arrow-right" viewBox="0 0 6 9">
+                <path
+                    d="M.646 8.854a.5.5 0 0 1 0-.708L4.293 4.5.646.854a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708 0Z"
+                />
+            </symbol>
+        </defs>
     </svg>
 </template>
