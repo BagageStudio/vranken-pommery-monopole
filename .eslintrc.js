3f57eb15adc1139d39da45a1544091f9c28e254c
Clément Lemoine <clementlemoine.cl@gmail.com>
2022-01-06 16:11:29 +0100

    🔧 eslint no-v-html off

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,23 @@ module.exports = {
     root: true,
     env: {
         browser: true,
-        node: true,
+        node: true
     },
     parserOptions: {
         parser: '@babel/eslint-parser',
-        requireConfigFile: false,
+        requireConfigFile: false
     },
     extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
     plugins: [],
     // add your custom rules here
     rules: {
         'no-console': 'off',
+        'vue/no-v-html': 'off',
         'no-unused-vars': 'warn',
         'no-useless-escape': 'off',
         quotes: ['error', 'single'],
         'object-curly-spacing': ['error', 'always'],
         'array-bracket-spacing': ['error', 'never'],
-        'arrow-parens': ['error', 'as-needed'],
-    },
-}
+        'arrow-parens': ['error', 'as-needed']
+    }
+};
