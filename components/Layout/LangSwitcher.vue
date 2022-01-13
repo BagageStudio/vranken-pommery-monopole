d4d484ad5c217c4052c777fa681b5644e051b473
Clément Lemoine <clementlemoine.cl@gmail.com>
2022-01-13 17:07:23 +0100

    💄 popin lang switcher

--- components/Layout/LangSwitcher.vue
+++ components/Layout/LangSwitcher.vue
@@ -5,7 +5,7 @@
                 {{ `${currentLocale.name} (${currentLocale.code})` }}<Icon name="arrow-right" />
             </button>
         </div>
-        <div v-show="showLang" class="other-lang">
+        <div :class="{ show: showLang }" class="other-lang">
             <div v-for="locale in availableLocales" :key="locale.code">
                 <a :href="switchLocalePath(locale.code)">
                     <span>{{ `${locale.name} (${locale.code})` }}</span>
@@ -69,6 +69,24 @@ export default {
 }
 .other-lang {
     position: absolute;
-    top: 100%;
+    top: calc(100% + 10px);
+    left: -20px;
+    white-space: nowrap;
+    padding: 15px 20px;
+    background-color: $beige;
+    border: 1px solid $grey-3;
+    opacity: 0;
+    transform: translateY(-15px);
+    pointer-events: none;
+    transition: 0.2s ease-out;
+    z-index: 1;
+    a {
+        text-decoration: none;
+    }
+    &.show {
+        opacity: 1;
+        transform: translateY(0);
+        pointer-events: all;
+    }
 }
 </style>
