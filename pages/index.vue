d2aa661609862108ff719fcda892f306b65f65a6
menthe <adrien.le.mentheour@gmail.com>
2022-01-28 11:42:29 +0100

    ✨ waiting page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <Tutorial/>
+  <div class="wrapper-page">
+    <img
+      width="268"
+      height="70"
+      loading="eager"
+      src="~static/img/vpm.png"
+      alt="Vranken Pommery"
+    />
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+.wrapper-page {
+  background-color: #f7f4ed;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
