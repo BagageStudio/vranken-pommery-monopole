5526ea3b4812fbfa0422d51f7b10a3265c447234
menthe <adrien.le.mentheour@gmail.com>
2022-06-13 15:30:44 +0200

    🛂 Google Site Verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,11 @@ export default {
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
+            { name: 'format-detection', content: 'telephone=no' },
+            {
+                name: process.env.GOOGLE_SITE_VERIFICATION_TOKEN ? 'google-site-verification' : '',
+                content: process.env.GOOGLE_SITE_VERIFICATION_TOKEN ? process.env.GOOGLE_SITE_VERIFICATION_TOKEN : ''
+            }
         ],
         link: [
             { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
